c4803649c5366a419f1f91bc47f7776e181632c6
rahman23 <rahman.hasanoglu@gmail.com>
2019-02-05 22:47:36 +0100

    Updates

---
+++ precache-manifest.a50a21df90390d8715314e2b9f632980.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c934484fba88479771fed59528a9000",
+    "url": "/demostore/static/media/banana.8c934484.jpg"
+  },
+  {
+    "revision": "2a899eac8ca4a56532a7",
+    "url": "/demostore/static/css/main.09cd8a43.chunk.css"
+  },
+  {
+    "revision": "082d5b837dfdb68d9270c675d304a0a3",
+    "url": "/demostore/static/media/watermelon.082d5b83.png"
+  },
+  {
+    "revision": "8669cd93ac1eb18aed9c",
+    "url": "/demostore/static/js/1.8669cd93.chunk.js"
+  },
+  {
+    "revision": "cc86a4684feaad2d4a2d",
+    "url": "/demostore/static/js/runtime~main.cc86a468.js"
+  },
+  {
+    "revision": "b449287f0fe3c3f180a62ac137c99fb8",
+    "url": "/demostore/static/media/Thumbs.b449287f.db"
+  },
+  {
+    "revision": "bb5bb0f496029b9ceb62a3a1fbdf94a9",
+    "url": "/demostore/static/media/ananas.bb5bb0f4.png"
+  },
+  {
+    "revision": "82da6769e78e7eecb5f1db520e74c500",
+    "url": "/demostore/static/media/apple.82da6769.jpg"
+  },
+  {
+    "revision": "2a899eac8ca4a56532a7",
+    "url": "/demostore/static/js/main.2a899eac.chunk.js"
+  },
+  {
+    "revision": "a2de4393bfa2faee59ed5f7d4d643f5a",
+    "url": "/demostore/static/media/coconut.a2de4393.png"
+  },
+  {
+    "revision": "3ce662e6be3b017b3f7fd115321e2317",
+    "url": "/demostore/static/media/lemon.3ce662e6.png"
+  },
+  {
+    "revision": "ece0f6ec52d06fcd8821a79435030b68",
+    "url": "/demostore/static/media/mango.ece0f6ec.jpg"
+  },
+  {
+    "revision": "0ff6bae5e3ffd6bc0697d3736ddd0ccd",
+    "url": "/demostore/static/media/orange.0ff6bae5.png"
+  },
+  {
+    "revision": "93424907b4dfdfb5a00a99d08423ed58",
+    "url": "/demostore/static/media/potato.93424907.png"
+  },
+  {
+    "revision": "0d87c42ac96c06da1dd9e460ecea8761",
+    "url": "/demostore/static/media/strawberry.0d87c42a.jpg"
+  },
+  {
+    "revision": "8669cd93ac1eb18aed9c",
+    "url": "/demostore/static/css/1.ffc147db.chunk.css"
+  },
+  {
+    "revision": "8aaa642bc00ee74b207d371e26e53112",
+    "url": "/demostore/index.html"
+  }
+];
